96f9446bbbf2b29b3b945ae40f2f5c3b1d57a034
prabas <prabasruck@gmail.com>
2021-07-10 10:59:15 +0545

    Initial Commit, Just ran startproject command

---
+++ RestaurantMS/RestaurantMS/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for RestaurantMS project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RestaurantMS.settings')
+
+application = get_wsgi_application()
