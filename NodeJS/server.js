c1cc396bc16d541ee254d650dccf3defc29da122
prabas <prabasruck@gmail.com>
2021-07-14 15:14:12 +0545

    Did sample project using NodeJS, ReactJS and MySql

---
+++ NodeJS/server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const mysql = require('mysql')
+
+// Create connection
+
+const db = mysql.createConnection({
+    host : 'localhost',
+    user: 'root',
+    password: "",
+    database: 'classicmodels',
+})
+
+db.connect((err)=>{
+    if(err){
+        console.error("Error on db connect");
+    }
+    console.log('MySQL connected'); 
+})
+  
+ 
+const app = express();
+
+app.get('/api/customers/', (req, res)=>{
+    let r = '';
+    db.query("SELECT * FROM customers", (err, rows, fields)=>{
+        if(err){
+            console.log('Error in the query');
+        }else{
+            console.log("Query successfull");
+            res.send(rows);
+            console.log("Done")
+        }
+    }) 
+}) 
+ 
+
+/*
+app.get('/createdb', (req, res)=>{
+    let sql = 'CREATE DATABASE nodeMysql'
+    db.query(sql, (err, res) =>{ 
+        if(err){ 
+            console.error("Error on query");
+        } 
+        else{
+            console.log(res);
+            res.send('Database Created');  
+        }
+    })
+})*/
+
+app.listen('4000', ()=>{ 
+    console.log('Server started on port 4000');
+}); 
---
+++ restaurantms/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import All from './js/All';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Restaurant Management system, ReactJS</h1>
+      <All></All>
+    </div>
+  );
+}
+
+export default App;
